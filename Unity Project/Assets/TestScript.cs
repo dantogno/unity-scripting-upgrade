51c2d3c3a5114186f1e4a130dec1245f5de0bf13
David Antognoli <david.antognoli@gmail.com>
2018-07-06 12:33:59 -0500

    adding demo project

---
+++ Unity Project/Assets/TestScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+}
