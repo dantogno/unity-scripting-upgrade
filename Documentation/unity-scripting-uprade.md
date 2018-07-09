6bfdbf39a6c312812c1a7844dd3d39e787885c9e
David Antognoli <david.antognoli@gmail.com>
2018-07-09 14:19:15 -0500

    WIP

--- Documentation/unity-scripting-uprade.md
+++ Documentation/unity-scripting-uprade.md
@@ -8,5 +8,9 @@ ms.assetid:
 ---
 # Unity Scripting Upgrade
 
-With the release of Unity 2017.1, Unity introduced an experimental version of its core scripting runtime upgraded to Mono/.NET 4.6 runtime. In Unity 2018.1, the .NET 4 equivalent runtime is no longer considered "experimental." Unity is projecting to make the upgraded scripting runtime the default selection in Unity 2018.3.
+## Introduction
+
+C# and .NET, the technologies underlying Unity scripting, have continued to receive updates since Microsoft originally released them in 2002. But Unity developers may not be aware of the steady stream of new features added to the C# language and .NET Framework. That's because prior to the release of Unity 2017.1, Unity has been using a .NET 3.5 equivalent scripting runtime, missing over 10 years of updates!
+
+With the release of Unity 2017.1, Unity introduced an experimental version of its core scripting runtime upgraded to a .NET 4.6 compatible version. In Unity 2018.1, the .NET 4.x equivalent runtime is no longer considered "experimental" and has been rebranded as "stable," while the older .NET 2 equivalent runtime is now considered "legacy." Unity is projecting to make the upgraded scripting runtime the default selection in Unity 2018.3.
 
--- Unity Project/Assets/TestScript.cs
+++ Unity Project/Assets/TestScript.cs
@@ -1,13 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Newtonsoft.Json;
+using SyntaxTree.VisualStudio.Bridge;
+//using Newtonsoft.Json;
+//using System.Net.http
 
 public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+        OptionalArgument();
+        Dynamic();
+        NamedArgument(message: " working");
+
+        string str = "Extension Method";
+        Debug.Log(str.ExtensionMethod());
+
+        // covariance & contravariance
+        Action<Base> b = (target) => { Debug.Log(target.GetType().Name + " covariance / contravariance worked"); };
+       // Action<Derived> d = b;
+       // d(new Derived());
+        string x = null;
+        string y = x ?? "null coalescing operator";
+        int? z = null;
+        int zz = z ?? 1;
+        Debug.Log("Nullable type" + zz);
+    }
+
+    private void NamedArgument(string message)
+    {
+        Debug.Log("named argument" + message);
+    }
+
+    private void OptionalArgument(string message = "optional argument working.")
+    {
+        Debug.Log(message);
+    }
+
+    private void Dynamic()
+    {
+        //dynamic dyn = "dynamic working";
+        //Debug.Log(dyn);
+    }
+
+
 }
+
+public static class ExtensionTest
+{
+    public static string ExtensionMethod(this string str)
+    {
+        return str + " is working";
+    }
+}
+
+public class Base { }
+public class Derived : Base { }
